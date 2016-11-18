230063886ab5e2b39355f0a03d88cbdb45bd0de0
Casey Goodge <goodgec@gmail.com>
2016-11-18 08:41:19 -0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <dir id='app'>
+    <Tiger/>
+  </dir>
+</template>
+
+<script type='text/javascript'>
+  import Tiger from './components/Tiger'
+  export default {
+    components: {
+      Tiger
+    }
+  }
+</script>
+
+<style type='text/css'>
+
+</style>
---
+++ src/components/Tiger.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class='tiger'>
+    <input type='text' name='name' v-model='searchText' @keypress='search'></input>
+    <ul>
+      <li v-for='result in results'>
+        {{ result.snippet.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script type='text/javascript'>
+  import axios from 'axios'
+
+  export default {
+    data () {
+      return {
+        rootUrl: 'https://www.googleapis.com/youtube/v3/search',
+        searchText: '',
+        key: 'AIzaSyCy9xazg0-n4M6D1V0yJoQsedSPF-7hdXs',
+        results: []
+      }
+    },
+    computed: {
+      params () {
+        return {
+          part: 'snippet',
+          key: this.key,
+          q: this.searchText,
+          type: 'video'
+        }
+      }
+    },
+    methods: {
+      search () {
+        if (!this.key) {
+          throw new Error('need YouTube API key')
+        }
+        axios.get(this.rootUrl, {
+          params: this.params
+        })
+        .then(response => {
+          this.results = response.data.items
+        })
+      }
+    }
+  }
+</script>
+
+<style type='text/css'>
+  .tiger {
+    font-weight: bold;
+  }
+</style>
